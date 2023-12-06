a46e6949613b93ee35f34bb71211cabeefa42b4c
K1RM1T <152967750+K1RM1T@users.noreply.github.com>
2023-12-06 17:37:39 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
+const fetchYandexData = (token) =>
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+    (res) => res.json()
+  );
+
 window.onload = () => {
-  document.getElementById("button").onclick = () => {
-    window.YaAuthSuggest.init(
+  document.getElementById("suggest").onclick = () => {
+    YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "86fd3489485d4ef6ae2ff608a86640dc",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-master-class-git-main-kirmits-projects.vercel.app/token.html",
       },
-      "https://examplesite.com",
+      "https://oauth-master-class-git-main-kirmits-projects.vercel.app/",
       {
         view: "button",
         parentId: "buttonContainer",
@@ -18,7 +33,13 @@ window.onload = () => {
       }
     )
       .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
-      .catch((error) => console.log("Обработка ошибки", error));
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+        
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так", error));
   };
 };
